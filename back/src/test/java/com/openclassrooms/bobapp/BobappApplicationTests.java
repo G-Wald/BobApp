61ae8a0d117daee44589af82ba767d5e34ba2c3c
G-Wald <gauthier_manigand@hotmail.fr>
2023-11-27 16:28:31 +0100

    test qui marche pas

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -3,16 +3,15 @@ package com.openclassrooms.bobapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class BobappApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
+		assertEquals(1, 2);
 
-	@Test
-	void testquivarater(){
-		assertEquals(1, 2));
 	}
 
 }
